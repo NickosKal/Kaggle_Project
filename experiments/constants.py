af02c0a4fea0d2812fc3803ef611985a0b69b62d
onuryuxcodes <yksl.onr@gmail.com>
2023-06-02 18:44:02 +0200

    Update constants.py

--- experiments/constants.py
+++ experiments/constants.py
@@ -2,9 +2,10 @@ DIAGNOSIS_COL = "Diagnosis"
 NAME_COL = "Name"
 
 ALL_TRAIN_FOLDER = "/Users/onur/Desktop/train_all_in"
-NEW_TRAIN_FOLDER_POS = "/Users/onur/Desktop/train/positive"
-NEW_TRAIN_FOLDER_NEG = "/Users/onur/Desktop/train/negative"
-NEW_TRAIN_FOLDER = "/Users/onur/Desktop/train"
+
+NEW_TRAIN_FOLDER = "/Users/onur/Desktop/train_mini3"
+NEW_TRAIN_FOLDER_POS = NEW_TRAIN_FOLDER+"/positive"
+NEW_TRAIN_FOLDER_NEG = NEW_TRAIN_FOLDER+"/negative"
 TEST_FOLDER = "/Users/onur/Desktop/test"
 MODEL_SAVE_PATH = "saved_model"
 RESULT_FOLDER = "/result"
@@ -12,4 +13,5 @@ TEST_FOLDER_TEST = "/Users/onur/Desktop/test_to_test"
 
 image_size = (128, 128)
 image_size_nn = (128, 128, 3)
+image_size_gray_scale = (128, 128, 1)
 
