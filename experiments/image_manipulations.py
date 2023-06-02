a6d39a3c7c04b208042d0395495d89513a86ebd8
onuryuxcodes <yksl.onr@gmail.com>
2023-06-02 18:43:36 +0200

    Update image_manipulations.py

--- experiments/image_manipulations.py
+++ experiments/image_manipulations.py
@@ -1,8 +1,8 @@
 import os
-import cv2
+#import cv2
 import random
 
-
+'''
 def occurrence_of_given_probability(probability):
     percentile = probability * 100
     sample = random.randint(0, 99)
@@ -22,14 +22,40 @@ def rotate_image_randomly_90x_degrees(img):
     return rotated_img
 
 
+def add_gaussian_blurr(img):
+    possible_kernels = [(11, 11), (5, 5), (3, 3), (7, 7), (13, 13)]
+    blurred = cv2.GaussianBlur(img, random.choice(possible_kernels), 0)
+    return blurred
+
+
 def read_all_images_in_path_and_with_a_probability_rotate_and_save(path_to_folder, probability):
     os.chdir(path_to_folder)
     count_of_rotation = 0
+    count = 0
     for image_file_name in os.listdir():
         image_incoming = read_img_from_path(image_file_name)
+        count = count + 1
         if (image_incoming is not None) and (occurrence_of_given_probability(probability)):
             print("rotating")
             rotated = rotate_image_randomly_90x_degrees(image_incoming)
             cv2.imwrite("rotated_"+image_file_name, rotated)
             count_of_rotation = count_of_rotation + 1
     print("rotated "+str(count_of_rotation)+" images")
+    print("in total "+str(count+count_of_rotation) + " images")
+
+
+def read_all_images_in_path_and_with_a_probability_add_blur_and_save(path_to_folder, probability):
+    os.chdir(path_to_folder)
+    count_of_blur = 0
+    count = 0
+    for image_file_name in os.listdir():
+        image_incoming = read_img_from_path(image_file_name)
+        count = count + 1
+        if (image_incoming is not None) and (occurrence_of_given_probability(probability)):
+            print("adding blur")
+            blurred = add_gaussian_blurr(image_incoming)
+            cv2.imwrite("blurred_"+image_file_name, blurred)
+            count_of_blur = count_of_blur + 1
+    print("augmented "+str(count_of_blur)+" images")
+    print("in total "+str(count+count_of_blur) + " images")
+'''
